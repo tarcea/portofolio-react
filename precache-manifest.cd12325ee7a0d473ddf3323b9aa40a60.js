b9d046bd935f882e6bdd42e009f7160a56f258ab
Gheorghe Tarcea <tarcea@gmail.com>
2020-06-22 15:15:40 +0200

    Updates

---
+++ precache-manifest.cd12325ee7a0d473ddf3323b9aa40a60.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68435761d51911ca6117d361650bfcb0",
+    "url": "/portofolio-react/index.html"
+  },
+  {
+    "revision": "61877462e9f1e873f019",
+    "url": "/portofolio-react/static/css/2.c01990bc.chunk.css"
+  },
+  {
+    "revision": "75dc2e032cc0295bd27b",
+    "url": "/portofolio-react/static/css/main.99b18a0b.chunk.css"
+  },
+  {
+    "revision": "61877462e9f1e873f019",
+    "url": "/portofolio-react/static/js/2.fe392c89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portofolio-react/static/js/2.fe392c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75dc2e032cc0295bd27b",
+    "url": "/portofolio-react/static/js/main.944dd5ba.chunk.js"
+  },
+  {
+    "revision": "e3c9712cdbba3f9af2b8",
+    "url": "/portofolio-react/static/js/runtime-main.d3a9c507.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portofolio-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portofolio-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portofolio-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portofolio-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portofolio-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2ec650e658e420abbf14fd0576193854",
+    "url": "/portofolio-react/static/media/gh.2ec650e6.jpeg"
+  }
+]);
